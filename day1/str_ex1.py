c1964d63aec6692960ea8d99a06b4b758e96c349
Anh Tran <hunganh72@gmail.com>
2017-10-17 09:05:38 -0700

    moving stuff from first-day to day1

---
+++ day1/str_ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+name = "Anh Tran"
+tel = "408.418.8264"
+email = "anh@dropbox.com"
+
+print ("{:>30}{:>30}{:>30}".format(name, tel, email))
+
+name2 = input("Enter your name: ")
+print (name2)
